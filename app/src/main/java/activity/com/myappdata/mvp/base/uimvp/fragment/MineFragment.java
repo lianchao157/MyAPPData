4de5cfda63f30d4c796b5a3f4d44355083e6567d
lianchao <18900945189@163.com>
2022-04-27 10:44:06 +0800

    1  DialogUtil 抽象接口使用2 拍照和打开相册

--- app/src/main/java/activity/com/myappdata/mvp/base/uimvp/fragment/MineFragment.java
+++ app/src/main/java/activity/com/myappdata/mvp/base/uimvp/fragment/MineFragment.java
@@ -1,13 +1,26 @@
 package activity.com.myappdata.mvp.base.uimvp.fragment;
 
+import android.Manifest;
+import android.app.Dialog;
 import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.graphics.drawable.Drawable;
+import android.net.Uri;
+import android.os.Build;
+import android.provider.MediaStore;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageView;
 import android.widget.LinearLayout;
 import android.widget.TextView;
+import android.widget.Toast;
 
+import java.io.File;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -23,20 +36,27 @@ import activity.com.myappdata.mvp.base.uimvp.adpter_mvp.PlaceAdapter;
 import activity.com.myappdata.mvp.base.uimvp.adpter_mvp.UserInfoAdapter;
 import activity.com.myappdata.mvp.base.utilsmvp.LogUtils;
 import activity.com.myappdata.mvp.base.view.IProvinceCallbask;
+import activity.com.myappdata.util.DialogUtil;
+import activity.com.myappdata.util.ToastUtil;
 
 /**
  * https://zhuanlan.zhihu.com/p/45701880
  * <p>
  * <p>
  * 自己的fragment
+ * <p>
+ * 图片裁剪和上传服务端
  */
 public class MineFragment extends BaseFragment implements IProvinceCallbask, View.OnClickListener {
 
+    private static final String TAG = "MineFragment";
     private TextView tv_mine;
     private LinearLayout lin_tv_mine;//  商品选择类型的布局
     private Button my_logoutbtn;//  登出系统
     private LinearLayout setpowerlin; //设置预留
-
+    private ImageView img_head;// 顶部的图片
+    private static final String FILE_PATH = "/sdcard/syscamera.jpg";// 照片路径
+    public  static   int OPEN_CANMER=99;
     @Override
     protected int getLayoutInflaterResId() {
         return R.layout.fragment_item_goods_config_info;
@@ -78,6 +98,9 @@ public class MineFragment extends BaseFragment implements IProvinceCallbask, Vie
                 startActivity(i);
             }
         });
+//        顶部图片裁剪
+        img_head = (ImageView) rootView.findViewById(R.id.c1);
+        img_head.setOnClickListener(this);
     }
 
     @Override
@@ -103,6 +126,101 @@ public class MineFragment extends BaseFragment implements IProvinceCallbask, Vie
     @Override
     public void onClick(View v) {
         switch (v.getId()) {
+            case R.id.img_head:
+                //加载本地drawable文件
+                Drawable mybird1 = getResources().getDrawable(R.drawable.default_head);
+                img_head.setImageDrawable(mybird1);
+                break;
+            case R.id.c1:
+                DialogUtil.showCentreDialogbyBoom(MineFragment.this.getActivity());
+                DialogUtil dialogUtil = new DialogUtil();
+                dialogUtil.buttonClickEvent(new DialogUtil.DialogButtonClick() {
+                    @Override
+                    public void cilckComfirmButton(View view) {
+                        if (Build.VERSION.SDK_INT >= 23) {
+                            int checkCallPhonePermission = ContextCompat.checkSelfPermission(MineFragment.this.getActivity(), Manifest.permission.CAMERA);
+                            if(checkCallPhonePermission != PackageManager.PERMISSION_GRANTED){
+                                ActivityCompat.requestPermissions(MineFragment.this.getActivity(),new String[]{Manifest.permission.CAMERA},99);
+                                return;
+                            }else{
+                                showCamera();
+                            }
+                        } else {
+                            showCamera();
+                        }
+//                        原文链接：https://blog.csdn.net/dubo_csdn/article/details/81743212
+
+                    }
+
+                    @Override
+                    public void cilckCancleButton(View view) {
+                        Intent intent = new Intent();
+                        // 指定开启系统相机的Action
+                        intent.setAction(MediaStore.ACTION_IMAGE_CAPTURE);
+                        intent.addCategory(Intent.CATEGORY_DEFAULT);
+                        startActivityForResult(intent, 1);
+                    }
+                });
+
+                break;
+            case 99:
+
+                break;
+        }
+    }
+
+    private void showCamera() {
+        Intent intent = new Intent(Intent.ACTION_PICK, null);
+        intent.setDataAndType(MediaStore.Images.Media.EXTERNAL_CONTENT_URI, "image/*");
+        startActivityForResult(intent, 2);
+
+    }
+
+    /***
+     * 打开相册的方法
+     * @param requestCode
+     * @param resultCode
+     * @param data
+     */
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == 2) {
+            // 从相册返回的数据
+//            Log.e(this.getClass().getName(), "Result:" + data.toString());
+            if (data != null) {
+                // 得到图片的全路径
+                Uri uri = data.getData();
+                img_head.setImageURI(uri);
+                Log.e(this.getClass().getName(), "Uri:" + String.valueOf(uri));
+            }
+        } else if (requestCode == 0) {
+            File file = new File(FILE_PATH);
+            Uri uri = Uri.fromFile(file);
+            img_head.setImageURI(uri);
+        } else if (requestCode == 1) {
+//            Log.i(TAG, "默认content地址：" + data.getData());
+            if(null==data||null==data.getData()){
+                ToastUtil.makeText(MineFragment.this.getContext(),"你还没有选择图片哦");
+            }else{
+                img_head.setImageURI(data.getData());
+            }
+
+        }
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, String[] permissions, int[] grantResults) {
+        switch (requestCode) {
+            case 99:
+                if (grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                    showCamera();
+                } else {
+                    Toast.makeText(MineFragment.this.getActivity(), "相机权限禁用了。请务必开启相机权", Toast.LENGTH_SHORT).show();
+                }
+                break;
+            default:
+                super.onRequestPermissionsResult(requestCode, permissions, grantResults);
         }
     }
 }
--- app/src/main/java/activity/com/myappdata/util/DialogUtil.java
+++ app/src/main/java/activity/com/myappdata/util/DialogUtil.java
@@ -3,7 +3,17 @@ package activity.com.myappdata.util;
 import android.app.Dialog;
 import android.content.Context;
 import android.content.DialogInterface;
+import android.content.Intent;
+import android.provider.MediaStore;
 import android.support.v7.app.AlertDialog;
+import android.view.Gravity;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.Window;
+import android.view.WindowManager;
+import android.widget.TextView;
+
+import activity.com.myappdata.R;
 
 /**
  * Created by YuShuangPing on 2018/11/5.   Android  6.0  的dialog  申请权限
@@ -42,14 +52,100 @@ public class DialogUtil {
                 .create();
         return dialog;
     }
- 
- 
+
+
+
     public interface DialogClickListener {
  
         public abstract void confirm();
  
         public abstract void cancel();
  
+    }
+//    底部显示dialog的方法
+    private static Dialog dialog;
+    private static View inflate;
+    static TextView tv_take_pic;
+    static  TextView tv_take_photo;
+    //中间显示的dialog
+    public static void showCentreDialogbyBoom(Context context) {
+        //自定义dialog显示布局
+        inflate = LayoutInflater.from(context).inflate(R.layout.showlayoutdialoge, null);
+        tv_take_photo= (TextView) inflate.findViewById(R.id.tv_take_photo);
+        tv_take_photo.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+
+         tv_take_pic= (TextView) inflate.findViewById(R.id.tv_take_pic);
+        tv_take_pic.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+
+        TextView tv_cancel= (TextView) inflate.findViewById(R.id.tv_cancel);
+        tv_cancel.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                DialogUtil.close();
+            }
+        });
+        //自定义dialog显示风格
+        dialog = new Dialog(context, R.style.DialogCentre);
+        //点击其他区域消失
+        dialog.setCanceledOnTouchOutside(true);
+        dialog.setContentView(inflate);
+        Window window = dialog.getWindow();
+        WindowManager.LayoutParams wlp = window.getAttributes();
+        wlp.gravity = Gravity.CENTER;
+        wlp.width = WindowManager.LayoutParams.WRAP_CONTENT;
+        window.setAttributes(wlp);
+        dialog.show();
+    }
+
+    //关闭dialog时调用
+    public static void close() {
+        if (dialog != null) {
+            dialog.dismiss();
+            dialog = null;
+        }
+    }
+    AlertDialog alertDialog;
+
+    private DialogButtonClick mClick;
+    public interface DialogButtonClick {
+        void cilckComfirmButton(View view);
+        void cilckCancleButton(View view);
+    }
+
+    public void buttonClickEvent(DialogButtonClick bc){
+        if(bc != null){
+            mClick = bc;
+            if (tv_take_pic != null) {
+                tv_take_pic.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        mClick.cilckCancleButton(v);
+                    }
+                });
+                tv_take_photo.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        mClick.cilckComfirmButton(v);
+                    }
+                });
+            }
+            cilckEvent();
+        }
+    }
+
+    private void cilckEvent() {
+
+
     }
 }
 //————————————————
