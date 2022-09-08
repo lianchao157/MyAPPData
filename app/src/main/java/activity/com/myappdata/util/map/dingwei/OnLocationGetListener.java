244dc062b3e6b34626ad872d8567b34963ff2843
lianchao <18900945189@163.com>
2022-09-08 18:50:51 +0800

    有些mvp的布局   高德地图点击监听

---
+++ app/src/main/java/activity/com/myappdata/util/map/dingwei/OnLocationGetListener.java
@@ -0,0 +1,10 @@
+package activity.com.myappdata.util.map.dingwei;
+
+import activity.com.myappdata.lib.PositionEntity;
+
+public interface OnLocationGetListener {
+
+	public void onLocationGet(PositionEntity entity);
+	
+	public void onRegecodeGet(PositionEntity entity);
+}
