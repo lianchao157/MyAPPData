2bfaacb6bc0eca3dbef038e86dd1f2edbad6ce8e
lianchao <18900945189@163.com>
2022-09-09 11:14:23 +0800

    高德地图

--- app/src/main/java/activity/com/myappdata/mvp/base/utilsmvp/Constants.java
+++ app/src/main/java/activity/com/myappdata/mvp/base/utilsmvp/Constants.java
@@ -3,4 +3,22 @@ package activity.com.myappdata.mvp.base.utilsmvp;
 public class Constants {
     public static final String BASE_URL1 = "http://192.168.1.5:8988/";
     public static final String BASE_URL = "http://guolin.tech/api/";
+
+
+//      与微信相关的代码url
+
+
+    //发布上线改为true
+    public static final boolean RELEASE_MODE = true;
+
+    // 微信相关
+    public static final String APP_ID = "wx8d20b87b8cc47cc5";
+
+    public static final int PERMISSIONS_REQUEST_STORAGE = 1;
+
+    public static class ShowMsgActivity {
+        public static final String STitle = "showmsg_title";
+        public static final String SMessage = "showmsg_message";
+        public static final String BAThumbData = "showmsg_thumb_data";
+    }
 }
