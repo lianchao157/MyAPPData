e2a44969a7169e27f0eb0666d3ed82324316efa2
lianchao <18900945189@163.com>
2022-09-09 11:40:40 +0800

    高德地图 微信登陆代码测试

---
+++ app/src/main/java/activity/com/myappdata/WEIxinLoginTestActivity.java
@@ -0,0 +1,13 @@
+package activity.com.myappdata;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class WEIxinLoginTestActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_w_e_ixin_login_test);
+    }
+}
